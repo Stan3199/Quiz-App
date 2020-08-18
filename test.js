17c79491100f267cc70db814974f382d592d8f6d
Stan3199 <piyush.nautiyal85@gmail.com>
2020-08-18 23:07:40 +0530

    second last commit

--- test.js
+++ test.js
@@ -1,4 +1,46 @@
 var Test;
 (function (Test) {
-    console.log(JSON.parse(localStorage.getItem("page1")));
+    var Optionss = /** @class */ (function () {
+        function Optionss(url) {
+            this.url = url;
+        }
+        return Optionss;
+    }());
+    Test.Optionss = Optionss;
+    var res = /** @class */ (function () {
+        function res() {
+            this.result = function (resu) {
+                if (resu === void 0) { resu = new Optionss("./answers.json"); }
+                var count = 0;
+                $.ajax({
+                    url: resu.url,
+                    type: "get",
+                    success: function (data) {
+                        var andata = data.answers;
+                        for (var i = 1; i < 6; i++) {
+                            var page = "page" + i;
+                            var lodata = JSON.parse(localStorage.getItem(page));
+                            for (var pdata in andata) {
+                                var pagedata = andata[pdata];
+                                for (var quest in pagedata) {
+                                    // console.log(pagedata[quest])
+                                    // console.log(lodata[quest])
+                                    if (JSON.stringify(pagedata[quest]) == JSON.stringify(lodata[quest])) {
+                                        count++;
+                                        console.log(count);
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    error: function (error) {
+                    }
+                });
+            };
+        }
+        return res;
+    }());
+    Test.res = res;
+    var result = new res();
+    result.result();
 })(Test || (Test = {}));
--- test.ts
+++ test.ts
@@ -7,10 +7,12 @@ module Test{
     export class res{
         
         public result = (resu: Optionss=new Optionss("./answers.json")) : void =>{
+            var count=0;
             $.ajax({
                 url:resu.url,
                 type:"get",
                 success:function (data){
+                    var ansno=1;
                     var andata=data.answers;
                     for(var i:number =1;i<6;i++){
                         var page:string="page"+i;
@@ -18,7 +20,8 @@ module Test{
                         for(var pdata in andata){
                             var pagedata=andata[pdata];
                             for(var quest in pagedata){
-                                if(!pagedata[quest]==lodata[quest])
+                                $("#ans"+ansno).html()
+                                if(JSON.stringify(pagedata[quest])==JSON.stringify(lodata[quest])) {count++;console.log(count)}
 
                             }
                         }
